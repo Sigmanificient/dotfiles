b9947208e1319bcf2fa049c41debb5dbd83aad04
sigmanificient <edhyjox@gmail.com>
2022-09-17 16:14:00 +0100

    :sparkles: Add script to install fish

---
+++ scripts/fish.sh
@@ -0,0 +1,3 @@
+pacman -Syyu fish
+usermod -s /usr/bin/fish sigmanificient
+cp -r .config/fish ~/.config/fish
--- scripts/get_apps.sh
+++
@@ -1,1 +0,0 @@
-yay -S flameshot jetbrains-toolbox firefox-developer-edition kitty discord-canary sublime-text-4 dolphin ark
