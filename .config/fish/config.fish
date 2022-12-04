3cb03697ef931347a12f43b8fb133d79a5e4fe76
sigmanificient <edhyjox@gmail.com>
2022-12-04 20:52:01 +0100

    :fire: Remove fish

--- .config/fish/config.fish
+++
@@ -1,9 +0,0 @@
-function fish_greeting
-  if test -d ~/.local/share/JetBrains/Toolbox/shortcut
-    fish_add_path ~/.local/share/JetBrains/Toolbox/shortcut
-  end
-
-  if status is-interactive
-    neofetch
-  end
-end
