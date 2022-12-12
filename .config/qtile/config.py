d36fb84dca8eff7727ef17d271b94b3c6dcfb5a8
sigmanificient <edhyjox@gmail.com>
2022-12-12 12:48:21 +0100

    :wrench: Remove layout switch

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -42,11 +42,6 @@ keys = [
 
     # A list of available commands that can be bound to keys can be found
     # at https://docs.qtile.org/en/latest/manual/config/lazy.html
-    # Switch between windows
-    Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
-    Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
-    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
-    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
     Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
