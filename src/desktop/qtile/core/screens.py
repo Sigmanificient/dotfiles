f77f78df44e4e9cb26e9d83510a2a025662f451f
sigmanificient <edhyjox@gmail.com>
2022-12-14 16:46:41 +0100

    :recycle: Replace Nitrogen with Qtile Wallpaper system

--- src/desktop/qtile/core/screens.py
+++ src/desktop/qtile/core/screens.py
@@ -1,18 +1,25 @@
+import os
+
 from libqtile.config import Screen
 
 from core.bar import create_bar, gap
 
+wallpaper_path = os.path.expanduser("~/Pictures/wallpaper.jpg")
 screens = [
     Screen(
         top=create_bar(),
         bottom=gap(),
         left=gap(),
         right=gap(),
+        wallpaper=wallpaper_path,
+        wallpaper_mode="fill",
     ),
     Screen(
         top=create_bar(secondary=True),
         bottom=gap(),
         left=gap(),
         right=gap(),
-    )
+        wallpaper=wallpaper_path,
+        wallpaper_mode="fill",
+    ),
 ]
