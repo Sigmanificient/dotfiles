75c3b1c51fca7d8e4154ba1a4e3fcae61af52c3e
sigmanificient <edhyjox@gmail.com>
2022-12-15 23:43:30 +0100

    :wrench: Add brightness control

--- src/desktop/qtile/core/keys.py
+++ src/desktop/qtile/core/keys.py
@@ -75,4 +75,13 @@ keys = [
         lazy.spawncmd(),
         desc="Spawn a command using a prompt widget"
     ),
+    # Backlight
+    Key(
+        [], "XF86MonBrightnessUp",
+        lazy.spawn("brightnessctl set +5%")
+    ),
+    Key(
+        [], "XF86MonBrightnessDown",
+        lazy.spawn("brightnessctl set 5%-")
+    )
 ]
