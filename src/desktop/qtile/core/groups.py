0db80b6fb3807e3e6ad5411e431758c02563fcb8
sigmanificient <edhyjox@gmail.com>
2022-12-21 19:50:55 +0100

    :wrench: Fix keybind group for FR keyboard

--- src/desktop/qtile/core/groups.py
+++ src/desktop/qtile/core/groups.py
@@ -4,22 +4,33 @@ from libqtile.lazy import lazy
 from .keys import keys, mod
 
 groups = [Group(f"{i}") for i in "ζδωχλξπσς"]
+group_keys = [
+    "ampersand",
+    "eacute",
+    "quotedbl",
+    "apostrophe",
+    "parenleft",
+    "minus",
+    "egrave",
+    "underscore",
+    "agrave",
+]
 
-for key, i in enumerate(groups, start=1):
+for (g, key) in zip(groups, group_keys):
     keys.extend(
         [
             # mod1 + letter of group = switch to group
             Key(
                 [mod],
-                str(key),
-                lazy.group[i.name].toscreen(),
-                desc="Switch to group {}".format(i.name),
+                key,
+                lazy.group[g.name].toscreen(),
+                desc="Switch to group {}".format(g.name),
             ),
             Key(
                 [mod, "shift"],
-                str(key),
-                lazy.window.togroup(i.name, switch_group=True),
-                desc="Switch to & move focused window to group {}".format(i.name),
+                key,
+                lazy.window.togroup(g.name, switch_group=True),
+                desc="Switch to & move focused window to group {}".format(g.name),
             )
         ]
     )
