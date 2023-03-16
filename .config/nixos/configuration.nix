2ffeebb7307443310f7a527e83c8b6d01af09a7f
sigmanificient <edhyjox@gmail.com>
2023-03-16 21:07:01 +0100

    :wrench: Switch to pulseaudio with alsa lib

--- .config/nixos/configuration.nix
+++ .config/nixos/configuration.nix
@@ -63,7 +63,13 @@
   };
 
   sound.enable = true;
-  hardware.pulseaudio.enable = true;
+  hardware.pulseaudio.enable = false;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
 
   programs.zsh = {
     enable = true;
