010a5b2a4e530f4009bf2ccfecacb60595d587d7
sigmanificient <edhyjox@gmail.com>
2023-03-16 14:42:41 +0100

    :wrench: Add gnome polkit

--- .config/nixos/configuration.nix
+++ .config/nixos/configuration.nix
@@ -163,6 +163,23 @@
      fade = true;
   };
 
+  security.polkit.enable = true;
+  systemd = {
+    user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = [ "graphical-session.target" ];
+      wants = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      serviceConfig = {
+          Type = "simple";
+          ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+          Restart = "on-failure";
+          RestartSec = 1;
+          TimeoutStopSec = 10;
+        };
+    };
+  };
+
   system.copySystemConfiguration = false;
   system.stateVersion = "22.11";
 }
