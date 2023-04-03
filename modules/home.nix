3a30e73d4e012fa7377e7d2bebb72238b23957b1
Sigmanificient <edhyjox@gmail.com>
2023-04-04 00:46:12 +0200

    :wrench: Upgrade firefox settings

--- modules/home.nix
+++ modules/home.nix
@@ -142,7 +142,43 @@
 
     firefox = {
       enable = true;
-      package = pkgs.firefox-devedition-bin;
+      package = pkgs.wrapFirefox pkgs.firefox-bin-unwrapped {
+        extraPolicies = {
+          CaptivePortal = false;
+          DisableFirefoxStudies = true;
+          DisablePocket = true;
+          DisableTelemetry = true;
+          DisableFirefoxAccounts = true;
+          NoDefaultBookmarks = true;
+          OfferToSaveLogins = false;
+          OfferToSaveLoginsDefault = false;
+          PasswordManagerEnabled = false;
+          FirefoxHome = {
+            Search = true;
+            Pocket = false;
+            Snippets = false;
+            TopSites = false;
+            Highlights = false;
+          };
+          UserMessaging = {
+            ExtensionRecommendations = false;
+            SkipOnboarding = true;
+          };
+        };
+      };
+      profiles = {
+        sigmanificient = {
+          id = 0;
+          name = "sigmanificient";
+          search = {
+            force = true;
+            default = "DuckDuckGo";
+          };
+          settings = {
+            "general.smoothScroll" = true;
+          };
+        };
+      };
     };
 
     zsh = import ./components/zsh.nix {};
