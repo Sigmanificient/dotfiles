13a11a911f77f75b0f8fe4b0b23df80502359d29
Sigmanificient <edhyjox@gmail.com>
2023-04-27 04:22:02 +0200

    :wrench: Remove editorconfig (didn't get it to work)

--- modules/home.nix
+++ modules/home.nix
@@ -21,24 +21,6 @@
       ../config/kitty.nix
     ];
 
-  editorconfig = {
-    enable = true;
-    settings = {
-      "*" = {
-        charset = "utf-8";
-        end_of_line = "lf";
-        trim_trailing_whitespace = true;
-        insert_final_newline = true;
-        max_line_length = 80;
-        indent_style = "space";
-        indent_size = 4;
-      };
-      "*.nix" = {
-        indent_size = 2;
-      };
-    };
-  };
-
   home = {
     username = "sigmanificient";
     homeDirectory = "/home/sigmanificient";
