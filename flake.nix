6ad2322b575b39810140fdc4b3927bec5e4c6663
Sigmanificient <edhyjox@gmail.com>
2023-04-30 18:03:23 +0200

    :wrench: Move machines declaration in machines/default.nix

--- flake.nix
+++ flake.nix
@@ -1,15 +1,15 @@
 {
   description = "Sigmachine configuration & dotfiles";
   inputs = {
-      nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
-      nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-      hosts.url = github:StevenBlack/hosts;
-      nixos-hardware.url = "github:NixOS/nixos-hardware";
-      home-manager = {
-        url = "github:nix-community/home-manager";
-        inputs.nixpkgs.follows = "nixpkgs";
-      };
+    hosts.url = github:StevenBlack/hosts;
+    nixos-hardware.url = "github:NixOS/nixos-hardware";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
@@ -31,42 +31,15 @@
 
   in
   {
-    nixosConfigurations.BaconServer = nixpkgs.lib.nixosSystem {
+    nixosConfigurations = (import ./machines {
+      inherit home-manager;
+      inherit hosts;
+      inherit nixpkgs;
+      inherit nixpkgs-unstable;
+      inherit nixos-hardware;
+      inherit self;
       inherit system;
-
-      modules = [
-        ./machines/server/hardware-configuration.nix
-        ./machines/server/configuration.nix
-
-        home-manager.nixosModules.home-manager {
-          home-manager.users.bacon = import ./machines/server/home.nix;
-        }
-      ];
-    };
-
-    nixosConfigurations.Sigmachine = nixpkgs.lib.nixosSystem {
-      inherit system;
-
-      modules = [
-        # Harware
-        ./machines/main/hardware-configuration.nix
-        nixos-hardware.nixosModules.asus-battery
-        nixos-hardware.nixosModules.common-cpu-amd
-        nixos-hardware.nixosModules.common-pc
-        nixos-hardware.nixosModules.common-pc-ssd
-
-        # System
-        (import ./machines/main/configuration.nix { inherit unstable; })
-        hosts.nixosModule {
-          networking.stevenBlackHosts.enable = true;
-        }
-
-        # Home
-        home-manager.nixosModules.home-manager {
-          home-manager.users.sigmanificient =
-            (import ./machines/main/home.nix { inherit unstable; });
-        }
-      ];
-    };
+      inherit unstable;
+    });
   };
 }
