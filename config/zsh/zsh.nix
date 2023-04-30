a2ca16a38c96af9c86cb24b27132a2a165c6266f
Sigmanificient <edhyjox@gmail.com>
2023-04-30 16:39:51 +0200

    :wrench: Use fixed zsh plugins

--- config/zsh/zsh.nix
+++ config/zsh/zsh.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.zsh = {
     enable = true;
@@ -7,6 +7,36 @@
     enableSyntaxHighlighting = true;
 
     initExtra = "source $HOME/.profile";
+
+    plugins = [
+      {
+        name = "zsh-syntax-highlighting";
+        src = pkgs.fetchFromGitHub {
+          owner = "zsh-users";
+          repo = "zsh-syntax-highlighting";
+          rev = "2d60a47cc407117815a1d7b331ef226aa400a344";
+          sha256 = "1pnxr39cayhsvggxihsfa3rqys8rr2pag3ddil01w96kw84z4id2";
+        };
+      }
+      {
+        name = "zsh-wakatime";
+        src = pkgs.fetchFromGitHub {
+          owner = "wbingli";
+          repo = "zsh-wakatime";
+          rev = "7396e143f2eb048a4a6d64dacae81c52e1ad72ab";
+          sha256 = "1pnxr39cayhsvggxihsfa3rqys8rr2pag3ddil01w96kw84z4id2";
+        };
+      }
+      {
+        name = "zsh-autocomplete";
+        src = pkgs.fetchFromGitHub {
+          owner = "marlonrichert";
+          repo = "zsh-autocomplete";
+          rev = "eee8bbeb717e44dc6337a799ae60eda02d371b73";
+          sha256 = "1pnxr39cayhsvggxihsfa3rqys8rr2pag3ddil01w96kw84z4id2";
+        };
+      }
+    ];
 
     shellAliases = {
       ll = "ls -l";
@@ -21,11 +51,6 @@
       plugins = [
         "git"
         "ssh-agent"
-        "wakatime"
-        "zsh-wakatime"
-        "zsh-autocomplete"
-        "zsh-syntax-highlighting"
-        "zsh-wakatime"
       ];
 
       theme = "sigma";
