2cfcfa4622b7e0b2547b0c7cb59b60faa480a232
Sigmanificient <edhyjox@gmail.com>
2023-07-02 05:01:08 +0200

    :wrench: Update flake inputs

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     nixos-hardware.url = "github:NixOS/nixos-hardware";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-23.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
