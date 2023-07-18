ccd74d5dfee6fc22c425431d341b88a2edd766ac
Sigmanificient <edhyjox@gmail.com>
2023-07-18 02:32:13 +0200

    :wrench: Increase cache duration

--- config/default.nix
+++ config/default.nix
@@ -20,8 +20,7 @@
   nix = {
     gc = {
       automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 7d";
+      options = "--delete-older-than 30d";
     };
     settings = {
       experimental-features = [ "nix-command" "flakes" ];
