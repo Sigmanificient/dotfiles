b23a59ec9d315a8e301b02c8e6959002b0643f4d
Sigmanificient <edhyjox@gmail.com>
2023-07-18 03:42:57 +0200

    :wrench: WIP Fix lockscreen

--- home/betterlockscreen.nix
+++ home/betterlockscreen.nix
@@ -1,9 +1,12 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
-  services.betterlockscreen.enable = true;
+  services.betterlockscreen = {
+    enable = true;
+    arguments = [ "-u ~/assets/lockscreen.png" ];
+  };
 
   home.file.lockscreen = {
     source = ./../assets/lockscreen.png;
-    target = "lockscreen.png";
+    target = "assets/lockscreen.png";
   };
 }
