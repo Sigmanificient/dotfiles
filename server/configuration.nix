1dff23677b96770926dc2f4ba06b26bbd3d01f46
Sigmanificient <edhyjox@gmail.com>
2023-08-02 23:46:21 +0200

    Add server config (blank)

---
+++ server/configuration.nix
@@ -0,0 +1,19 @@
+{ ... }: {
+  imports = [
+    ./hardware-configuration.nix
+
+
+  ];
+
+  boot.cleanTmpDir = true;
+  zramSwap.enable = true;
+  networking.hostName = "SigServer";
+
+  networking.domain = "";
+
+  services.openssh.enable = true;
+  users.users.root.openssh.authorizedKeys.keys = [
+    ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKVVrEl8ssTf4GcObE4MVlKRlzI3Ok7NX3unSv+TPrLG edhyjox@gmail.com''
+  ];
+}
+
