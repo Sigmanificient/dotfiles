795835fe371532472b95f0611813f2665e3c56d8
Sigmanificient <edhyjox@gmail.com>
2023-08-02 23:47:45 +0200

    Apply nixpkgs format

--- home/nvim/default.nix
+++ home/nvim/default.nix
@@ -1,29 +1,31 @@
 { pkgs, ... }:
-let vera = pkgs.stdenv.mkDerivation (finalAttrs: {
-  pname = "banana-vera";
-  version = "1.3.0-python3.10";
+let
+  vera = pkgs.stdenv.mkDerivation (finalAttrs: {
+    pname = "banana-vera";
+    version = "1.3.0-python3.10";
 
-  src = pkgs.fetchFromGitHub {
-    owner = "Epitech";
-    repo = "banana-vera";
-    rev = "refs/tags/v${finalAttrs.version}";
-    hash = "sha256-1nAKhUltQS1301JNrr0PQQrrf2W9Hj5gk1nbUhN4cXw=";
-  };
+    src = pkgs.fetchFromGitHub {
+      owner = "Epitech";
+      repo = "banana-vera";
+      rev = "refs/tags/v${finalAttrs.version}";
+      hash = "sha256-1nAKhUltQS1301JNrr0PQQrrf2W9Hj5gk1nbUhN4cXw=";
+    };
 
-  nativeBuildInputs = [ pkgs.cmake ];
-  buildInputs = with pkgs; [
-    python310
-    python310.pkgs.boost
-    tcl
-  ];
+    nativeBuildInputs = [ pkgs.cmake ];
+    buildInputs = with pkgs; [
+      python310
+      python310.pkgs.boost
+      tcl
+    ];
 
-  cmakeFlags = [
-    "-DVERA_LUA=OFF"
-    "-DVERA_USE_SYSTEM_BOOST=ON"
-    "-DPANDOC=OFF"
-  ];
-});
-in {
+    cmakeFlags = [
+      "-DVERA_LUA=OFF"
+      "-DVERA_USE_SYSTEM_BOOST=ON"
+      "-DPANDOC=OFF"
+    ];
+  });
+in
+{
   home.file.nvim_conf = {
     source = ./lua;
     target = ".config/nvim/lua";
