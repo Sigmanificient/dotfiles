c58d5d44c8f51f33f99acc92b0631bfcc8d4dd88
Sigmanificient <edhyjox@gmail.com>
2023-08-12 18:34:25 +0200

    Setup default keybind to focus nvim tree

--- home/nvim/lua/sigma/plugins/nvimtree.lua
+++ home/nvim/lua/sigma/plugins/nvimtree.lua
@@ -2,14 +2,7 @@ local function on_attach(bufnr)
     local api = require "nvim-tree.api"
 
     api.config.mappings.default_on_attach(bufnr)
+    vim.keymap.set("n", "<leader>t", "<cmd>NvimTreeFocus<CR>")
 end
 
-
-require("nvim-tree").setup({
-    on_attach = on_attach,
-    view = {
-        width = 30,
-    }
-})
-
-
+require("nvim-tree").setup({ on_attach = on_attach })
--- home/nvim/lua/sigma/remap.lua
+++ home/nvim/lua/sigma/remap.lua
@@ -1,2 +1,3 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
