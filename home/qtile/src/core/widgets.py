08704d52061970e65ef2e39d746c34cd16328177
Sigmanificient <edhyjox@gmail.com>
2023-08-19 15:39:01 +0200

    Fix Qtile Bar transparency

--- home/qtile/src/core/widgets.py
+++ home/qtile/src/core/widgets.py
@@ -10,7 +10,7 @@ widget_defaults = dict(
     font="JetBrainsMono Nerd Font",
     fontsize=12,
     padding=12,
-    background=Color.BG_DARK.with_alpha(0.7),
+    background=Color.BG_DARK.with_alpha(0.9),
     foreground=Color.TEXT_LIGHT,
 )
 
