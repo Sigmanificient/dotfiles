a83ecb8cdb420d71e56ee82e6a52426ad29c6b65
Sigmanificient <edhyjox@gmail.com>
2023-08-19 23:32:04 +0200

    Fix screen layout being inverted

--- home/qtile/src/autostart.sh
+++ home/qtile/src/autostart.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env sh
+
+[[ "$(hostname)" =~ 'Sigmachine' ]] \
+    && xrandr --output HDMI-0 --right-of DP-1 --primary
+
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 
 . "$HOME/.profile"
