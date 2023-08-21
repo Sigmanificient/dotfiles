54210fe640fa8fe95f4459714948666bdef1956e
Sigmanificient <edhyjox@gmail.com>
2023-08-21 13:38:43 +0200

    Add default if home wallpaper is not found

--- src/core/screens.py
+++ src/core/screens.py
@@ -1,3 +1,4 @@
+import pathlib
 import os
 
 from libqtile.bar import Gap
@@ -6,7 +7,13 @@ from libqtile.config import Screen
 from core.bar import Bar
 
 gap = Gap(4)
+
+_cwd = pathlib.Path(os.path.dirname(os.path.realpath(__file__)))
+
 wallpaper_path = os.path.expanduser("~/assets/wallpaper.png")
+
+if not os.path.exists(wallpaper_path):
+    wallpaper_path = str((_cwd / ".." / ".." / "wallpaper.png").absolute())
 
 screens = [
     Screen(
