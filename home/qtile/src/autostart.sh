9d7eea45e893488ce72b18cc27ecb4b78f79a84f
Sigmanificient <edhyjox@gmail.com>
2023-08-21 11:45:24 +0200

    Remove old polkit from arch

--- home/qtile/src/autostart.sh
+++ home/qtile/src/autostart.sh
@@ -3,8 +3,6 @@
 [[ "$(hostname)" =~ 'Sigmachine' ]] \
     && xrandr --output HDMI-0 --right-of DP-1 --primary
 
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-
 . "$HOME/.profile"
 
 notify-send "Welcome, $USER!" &
