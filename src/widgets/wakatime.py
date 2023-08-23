fa0096d9dba5a238f74fe326c1f171cdd48d9e51
Sigmanificient <edhyjox@gmail.com>
2023-08-23 19:31:38 +0200

    Add function to shorten wakatime text

--- src/widgets/wakatime.py
+++ src/widgets/wakatime.py
@@ -1,5 +1,6 @@
 """Wakatime widget by Drawu."""
 import subprocess
+import sys
 
 from libqtile import qtile
 from libqtile.widget import base
@@ -8,11 +9,11 @@ from libqtile.widget import base
 class Wakatime(base.InLoopPollText):
     def __init__(self, **config):
         self.name = "Wakatime widget"
-        self.default_string = ""
-
         super().__init__(
-            self.default_string, update_interval=600, qtile=qtile, **config
+            update_interval=600, qtile=qtile, **config
         )
+
+        self.default_string = "[:<]"
 
     def poll(self):
         try:
@@ -29,4 +30,30 @@ class Wakatime(base.InLoopPollText):
         if stderr is not None:
             return self.default_string
 
-        return " ".join(stdout.decode("utf-8").split()) or self.default_string
+        raw = stdout.decode("utf-8").strip()
+
+        if raw is not None:
+            return self.process_string(raw)
+
+        return self.default_string
+
+    @staticmethod
+    def process_string(raw) -> str:
+        activities = raw.split(', ')
+        out = []
+
+        for ac in activities:
+            if ac.count(' ') == 2:
+                m, _, ac_name = ac.split(' ')
+
+            elif ac.count(' ') == 4:
+                h, _, m, _, ac_name = ac.split(' ')
+                m = str(int(m) + (int(h) * 60))
+
+            else:
+                continue
+
+            out.append(f"{ac_name[0]}:{m}")
+
+        print("=>", ' '.join(out))
+        return ' '.join(out) 
