53d4ca2d41cbdce6f277ac1ca41804f8d5aad9db
Sigmanificient <edhyjox@gmail.com>
2023-09-07 22:57:17 +0200

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,9 +1,23 @@
 { pkgs ? import <nixpkgs> { }, ... }:
+let
+  py = {
+    env = pkgs.python311.withPackages (_: py.deps.all);
+    pkgs = pkgs.python311Packages;
+
+    deps = rec {
+      prod = with py.pkgs; [ qtile qtile-extras ];
+      dev = with py.pkgs; [ black isort vulture ];
+      all = prod ++ dev;
+    };
+  };
+
+in
 pkgs.mkShell {
   name = "Nix dotfiles";
   description = "Utils for my dotfiles.";
 
-  nativeBuildInputs = with pkgs; [
-    nixpkgs-fmt
-  ];
+  nativeBuildInputs = with pkgs; [ py.env deadnix ];
+  shellHook = ''
+    echo ${py.env}
+  '';
 }
