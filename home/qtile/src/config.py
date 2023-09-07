6ad2f4aecfc6ad588db3d9e6042ace9978ffdf2b
Sigmanificient <edhyjox@gmail.com>
2023-09-08 00:53:36 +0200

    Rewrite config.py

--- home/qtile/src/config.py
+++ home/qtile/src/config.py
@@ -1,47 +1,44 @@
-from core import autostart, floating_layout, groups, keys, layouts, mouse, screens
+from core import (
+    autostart,
+    floating_layout,
+    groups,
+    keys,
+    layouts,
+    mouse,
+    screens
+)
+
 from widgets import widget_defaults
 
-extension_defaults = widget_defaults.copy()
 
-dgroups_key_binder = None
-dgroups_app_rules = []
-follow_mouse_focus = True
-bring_front_click = False
-cursor_warp = False
+@lambda f: globals().update(f())
+def setup_qtile_globals():
+    return dict(
+        extension_defaults = widget_defaults.copy(),
 
-auto_fullscreen = True
-focus_on_window_activation = "smart"
-reconfigure_screens = True
+        dgroups_key_binder = None,
+        dgroups_app_rules = [],
 
-# If things like steam games want to auto-minimize themselves when losing
-# focus, should we respect this or not?
-auto_minimize = False
+        follow_mouse_focus = True,
+        bring_front_click = False,
+        cursor_warp = False,
 
-# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
-# string besides java UI toolkits; you can see several discussions on the
-# mailing lists, GitHub issues, and other WM documentation that suggest setting
-# this string if your java app doesn't work correctly. We may as well just lie
-# and say that we're a working one by default.
-#
-# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
-# java that happens to be on java's whitelist.
-wmname = "LG3D"
+        auto_fullscreen = True,
+        focus_on_window_activation = "smart",
+        reconfigure_screens = True,
+
+        auto_minimize = False,
+        wmname = "Qtile"
+    )
+
 
 __all__ = (
-    # Hooks
     "autostart",
-    # Keybindings
     "keys",
-    # Mouse
     "mouse",
-    # Workspaces groups
     "groups",
-    # Layouts
     "layouts",
     "floating_layout",
-    # Screens
     "screens",
-    # Widgets
     "widget_defaults",
-    "extension_defaults",
 )
