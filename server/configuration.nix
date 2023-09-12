986a7005a895787d4dc0ec1179aee42d35cdecec
Sigmanificient <edhyjox@gmail.com>
2023-09-12 16:48:14 +0200

    Add @drawbu as a maintainer of my server
    probly a bad idea...

--- server/configuration.nix
+++ server/configuration.nix
@@ -12,6 +12,7 @@
   services.openssh.enable = true;
   users.users.root.openssh.authorizedKeys.keys = [
     ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKVVrEl8ssTf4GcObE4MVlKRlzI3Ok7NX3unSv+TPrLG edhyjox@gmail.com''
+    ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMEIMRXQMnaP08FgRGXEpgX9oDADom5h+xjAnEgLNCXF clement2104.boillot@gmail.com''
   ];
 }
 
