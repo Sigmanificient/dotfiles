09c5fb16d596ba5bba184e9b3a81f5524b79ba6b
Sigmanificient <edhyjox@gmail.com>
2023-10-28 09:19:16 +0200

    Distinct shortcut for toggle & focus nvim tree

--- home/nvim/lua/sigma/plugins/nvimtree.lua
+++ home/nvim/lua/sigma/plugins/nvimtree.lua
@@ -2,7 +2,8 @@ local function on_attach(bufnr)
     local api = require "nvim-tree.api"
 
     api.config.mappings.default_on_attach(bufnr)
-    vim.keymap.set("n", "<leader>t", "<cmd>NvimTreeFocus<CR>")
+    vim.keymap.set("n", "<leader>t", "<cmd>NvimTreeToggle<CR>")
+    vim.keymap.set("n", "<leader>f", "<cmd>NvimTreeFocus<CR>")
 end
 
 require("nvim-tree").setup({ on_attach = on_attach })
