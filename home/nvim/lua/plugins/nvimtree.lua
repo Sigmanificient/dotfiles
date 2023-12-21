ddd30f708b33a14d977aed8b6ed02e988f5507dc
Sigmanificient <edhyjox@gmail.com>
2023-12-22 00:03:00 +0100

    Add tty friendly fallback for tree icons

--- home/nvim/lua/plugins/nvimtree.lua
+++ home/nvim/lua/plugins/nvimtree.lua
@@ -6,6 +6,43 @@ local function on_attach(bufnr)
   vim.keymap.set("n", "<leader>f", "<cmd>NvimTreeFocus<CR>")
 end
 
+local is_vt_tty = vim.fn.expand("$TERM") == "linux"
+
+local renderer_settings = {
+  [ false ] = nil,
+  [ true ] = {
+    icons = {
+      webdev_colors = false,
+      symlink_arrow = " → ",
+      glyphs = {
+        default = "#",
+        symlink = "@",
+        bookmark = ":",
+        modified = ".",
+        folder = {
+          arrow_closed = ">",
+          arrow_open = "v",
+          default = "/",
+          open = "/",
+          empty = "/",
+          empty_open = "/",
+          symlink = "@",
+          symlink_open = "@",
+        },
+        git = {
+          unstaged = "x",
+          staged = "&",
+          unmerged = "!",
+          renamed = "→",
+          untracked = "*",
+          deleted = "-",
+          ignored = ";",
+        },
+      }
+    }
+  }
+};
+
 require("nvim-tree").setup(
 {
   on_attach = on_attach,
@@ -13,5 +50,5 @@ require("nvim-tree").setup(
     git_ignored = false,
     custom = { "^\\.git$" },
   },
-}
-)
+  renderer = renderer_settings[is_vt_tty]
+})
