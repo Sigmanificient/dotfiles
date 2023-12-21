b2a0ccdf1ffda272e2acb584d301a67a6249705e
Sigmanificient <edhyjox@gmail.com>
2023-12-21 20:41:03 +0100

    Add editorconfig for lua

--- home/nvim/lua/sigma/plugins/toggleterm.lua
+++ home/nvim/lua/sigma/plugins/toggleterm.lua
@@ -1,17 +1,16 @@
 local Terminal  = require('toggleterm.terminal').Terminal
 
 local lazygit = Terminal:new({
-    cmd = 'lazygit || nix run nixpkgs#lazygit',
-    hidden = true,
-    direction = 'float',
-    on_open = function(term)
-        vim.api.nvim_buf_set_keymap(
-          term.bufnr,
-          'n', 'q', '<cmd>close<CR>',
-          {noremap = true, silent = true}
-        )
-    end,
+  cmd = 'lazygit || nix run nixpkgs#lazygit',
+  hidden = true,
+  direction = 'float',
+  on_open = function(term)
+    vim.api.nvim_buf_set_keymap(
+    term.bufnr,
+    'n', 'q', '<cmd>close<CR>',
+    {noremap = true, silent = true}
+    )
+  end,
 })
 
 vim.keymap.set('n', '<leader>gl', function() lazygit:toggle() end)
-
--- home/nvim/lua/sigma/remap.lua
+++ home/nvim/lua/sigma/remap.lua
@@ -1,3 +1,2 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-
