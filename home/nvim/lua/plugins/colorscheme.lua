5c25a777180feea6f13054e89716d33c293efd68
Sigmanificient <edhyjox@gmail.com>
2023-12-22 00:20:24 +0100

    Fix colorless nvim within tty

--- home/nvim/lua/plugins/colorscheme.lua
+++ home/nvim/lua/plugins/colorscheme.lua
@@ -1,2 +1,7 @@
-vim.cmd('colorscheme catppuccin-macchiato')
+local is_vt_tty = vim.fn.expand("$TERM") == "linux"
+
+if not is_vt_tty then
+  vim.cmd('colorscheme catppuccin-macchiato')
+end
+
 vim.api.nvim_set_hl(0, "Normal", { ctermbg = nil })
