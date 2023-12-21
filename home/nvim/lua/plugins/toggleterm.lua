9744852384c49bf76d771fed615a85d4aba1faf5
Sigmanificient <edhyjox@gmail.com>
2023-12-22 00:20:24 +0100

    Fix colorless nvim within tty

--- home/nvim/lua/plugins/toggleterm.lua
+++ home/nvim/lua/plugins/toggleterm.lua
@@ -6,9 +6,9 @@ local lazygit = Terminal:new({
   direction = "float",
   on_open = function(term)
     vim.api.nvim_buf_set_keymap(
-    term.bufnr,
-    "n", "q", "<cmd>close<CR>",
-    { noremap = true, silent = true }
+      term.bufnr,
+      "n", "q", "<cmd>close<CR>",
+      { noremap = true, silent = true }
     )
   end,
 })
