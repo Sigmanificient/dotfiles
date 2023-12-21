5bc84e8ce07e8a6fd3e74853ad4ca492d3cfd7d4
Sigmanificient <edhyjox@gmail.com>
2023-12-21 21:25:09 +0100

    Improve treesitter config

--- home/nvim/lua/sigma/plugins/treesitter.lua
+++ home/nvim/lua/sigma/plugins/treesitter.lua
@@ -1,18 +1,14 @@
-require'nvim-treesitter.configs'.setup {
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+require('nvim-treesitter.configs').setup {
+  ensure_installed = { "bash", "c", "lua", "vim", "python", "yaml" },
+  ignore_install = { "all" },
 
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  sync_install = false,
   auto_install = false,
 
   highlight = {
     enable = true,
-
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
   },
+
+  modules = {}
 }
