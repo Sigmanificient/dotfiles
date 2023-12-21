2dd5b4152734712e9a6a8d3d023eaac10541708d
Sigmanificient <edhyjox@gmail.com>
2023-12-21 17:26:01 +0100

    Refactor flake entry file

--- flake.nix
+++ flake.nix
@@ -4,13 +4,9 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
 
+    ecsls.url = "github:Sigmapitech-meta/ecsls";
     hosts.url = "github:StevenBlack/hosts";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
-
-    nix-index-database.url = "github:Mic92/nix-index-database";
-    nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
-
-    ecsls.url = "github:Sigmapitech-meta/ecsls";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-23.05";
@@ -18,67 +14,60 @@
     };
   };
 
-  outputs =
-    { nixpkgs
-    , nix-index-database
-    , nixos-hardware
-    , home-manager
-    , hosts
-    , ecsls
-    , ...
-    }:
-    let
-      username = "sigmanificient";
+  outputs = inputs:
+    with inputs; let
       system = "x86_64-linux";
-
       pkgs = nixpkgs.legacyPackages.${system};
-
-      default_modules = [
-        ./config
-
-        nix-index-database.nixosModules.nix-index
-
-        home-manager.nixosModules.home-manager
-        {
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            users.${username} = import ./home;
-            extraSpecialArgs = {
-              inherit username system ecsls;
-            };
-          };
-        }
-
-        hosts.nixosModule
-        {
-          networking.stevenBlackHosts.enable = true;
-        }
-      ];
-
     in
     {
       formatter.${system} = pkgs.nixpkgs-fmt;
 
-      nixosConfigurations = {
-        Bacon = nixpkgs.lib.nixosSystem {
-          inherit system;
+      nixosConfigurations =
+        let
+          username = "sigmanificient";
+        in
+        {
+          Bacon = nixpkgs.lib.nixosSystem {
+            inherit system;
 
-          specialArgs = {
-            inherit username;
-            hostname = "Bacon";
+            specialArgs = {
+              inherit username;
+              hostname = "Bacon";
+            };
+
+            modules =
+              let
+                mod-home-mananger = home-manager.nixosModules.home-manager {
+                  home-manager = {
+                    useGlobalPkgs = true;
+                    useUserPackages = true;
+                    users.${username} = import ./home;
+                    extraSpecialArgs = {
+                      inherit username system ecsls;
+                    };
+                  };
+                };
+
+                mod-hosts = hosts.nixosModule {
+                  networking.stevenBlackHosts.enable = true;
+                };
+
+                mod-nixhardware-lst = with nixos-hardware.nixosModules; [
+                  asus-battery
+                  common-cpu-amd
+                  common-pc
+                  common-pc-ssd
+                ];
+              in
+              [
+                ./config
+                ./hardware-configuration.nix
+                ./config/bacon.nix
+                mod-home-mananger
+                mod-hosts
+              ]
+              ++ mod-nixhardware-lst;
           };
-
-          modules = default_modules ++ [
-            ./hardware-configuration.nix
-            ./config/bacon.nix
-
-            nixos-hardware.nixosModules.asus-battery
-            nixos-hardware.nixosModules.common-cpu-amd
-            nixos-hardware.nixosModules.common-pc
-            nixos-hardware.nixosModules.common-pc-ssd
-          ];
         };
-      };
     };
 }
