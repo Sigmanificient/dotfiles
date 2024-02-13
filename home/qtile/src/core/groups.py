45647c6b75beff62779d9a18c40997e9ec94b519
Sigmanificient <edhyjox@gmail.com>
2024-02-13 10:35:38 +0100

    Update groups to reflect qwerty layout

--- home/qtile/src/core/groups.py
+++ home/qtile/src/core/groups.py
@@ -65,16 +65,6 @@ _scratchpads = [
 _Group.setup_single_keys()
 groups = _scratchpads + [
     _Group(lb, k) for lb, k in zip(
-        "ζπδωλσς", [
-            "ampersand",
-            "eacute",
-            "quotedbl",
-            "apostrophe",
-            "parenleft",
-            "minus",
-            "egrave",
-            "underscore",
-            "agrave",
-        ]
+        "ζπδωλσς", "1234567"
     )
 ]
