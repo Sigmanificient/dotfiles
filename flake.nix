4f0166ecd1e96cb657b6be822be964177eeab663
Sigmanificient <edhyjox@gmail.com>
2024-03-19 23:29:25 +0100

    Update flake to use nix 23.11 (broken)

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Sigma dotfiles";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     ecsls.url = "github:Sigmapitech/ecsls";
