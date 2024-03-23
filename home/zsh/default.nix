0f9d4a49ab0d957ca6048bec2537d6adc4fd0147
Sigmanificient <edhyjox@gmail.com>
2024-03-23 23:16:38 +0100

    Fix omz syntax higlighting key rename

--- home/zsh/default.nix
+++ home/zsh/default.nix
@@ -4,7 +4,7 @@
     enable = true;
     enableAutosuggestions = true;
     enableCompletion = true;
-    enableSyntaxHighlighting = true;
+    syntaxHighlighting.enable = true;
 
     plugins = [
       {
