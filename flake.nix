688bb4652d93dad652f0e3f9950f34295aa5e980
Sigmanificient <edhyjox@gmail.com>
2024-07-12 20:33:48 +0200

    Fix git hooks input

--- flake.nix
+++ flake.nix
@@ -37,11 +37,8 @@
     };
 
     pre-commit-hooks = {
-      url = "github:cachix/pre-commit-hooks.nix";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-utils.follows = "flake-utils";
-      };
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
