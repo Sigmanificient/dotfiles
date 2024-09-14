15ccd0048c71af9752ed4852b4c43215ab0d2ba2
Sigmanificient <edhyjox@gmail.com>
2024-09-14 17:14:56 +0200

    Fix build for qwerty-fr package

--- system/qwerty-fr.nix
+++ system/qwerty-fr.nix
@@ -7,14 +7,14 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     owner = "qwerty-fr";
     repo = "qwerty-fr";
     rev = "refs/tags/v${finalAttrs.version}";
-    sha256 = "sha256-BZLp5Tw1BH/m1wHYAUbfP86uZ6poAjD2D/uvl2ajmi0=";
+    hash = "sha256-TD67wKdaPaXzJzjKFCfRZl3WflUfdnUSQl/fnjr9TF8=";
   };
 
   installPhase = ''
     runHook preInstall
 
-    mkdir -p $out
-    cp -r usr/share $out/share
+    mkdir -p $out/share/X11/xkb/symbols
+    cp $src/linux/us_qwerty-fr $out/share/X11/xkb/symbols
 
     runHook postInstall
   '';
