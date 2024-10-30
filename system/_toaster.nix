78c97f33cd5da984bf312367ef226e542a41c400
Sigmanificient <edhyjox@gmail.com>
2024-10-30 20:14:26 +0100

    Shrink dependencies of Toaster

--- system/_toaster.nix
+++ system/_toaster.nix
@@ -1,4 +1,16 @@
-{ ... }: {
+{ lib, ... }: {
+  programs = {
+    noisetorch.enable = lib.mkForce false;
+    steam.enable = lib.mkForce false;
+  };
+
+  services.upower.enable = true;
+
+  virtualisation = {
+    docker.enable = lib.mkForce false;
+    libvirtd.enable = lib.mkForce false;
+  };
+
   system = {
     copySystemConfiguration = false;
     stateVersion = "24.05";
