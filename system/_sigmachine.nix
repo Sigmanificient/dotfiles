abcd44cf583296c1c1dfe283e3f850bec5beb4a9
Sigmanificient <edhyjox@gmail.com>
2024-12-03 22:45:35 +0100

    Remove autofreq from sigmachine

--- system/_sigmachine.nix
+++ system/_sigmachine.nix
@@ -1,23 +1,4 @@
 { config, pkgs, ... }: {
-  services = {
-    auto-cpufreq = {
-      enable = true;
-      settings = {
-        battery = {
-          governor = "powersave";
-          turbo = "never";
-        };
-
-        charger = {
-          governor = "performance";
-          turbo = "auto";
-        };
-      };
-    };
-
-    upower.enable = true;
-  };
-
   system = {
     copySystemConfiguration = false;
     stateVersion = "24.05";
