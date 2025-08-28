63fe517120c26aa2e7e7e5963b8b8139e1844692
Sigmanificient <edhyjox@gmail.com>
2025-08-28 23:58:04 +0200

    Move back to firefox & vesktop

--- home/qtile/src/controls.py
+++ home/qtile/src/controls.py
@@ -26,7 +26,7 @@ keys = [
     Key([mod, "shift"], "v", lazy.spawn("pavucontrol")),
     Key([mod], "l", lazy.spawn("betterlockscreen -l")),
     Key([mod], "f", lazy.window.toggle_floating()),
-    Key([mod], "b", lazy.spawn("floorp")),
+    Key([mod], "b", lazy.spawn("firefox")),
     Key([], "Print", lazy.spawn("flameshot gui --clipboard")),
     Key([mod], "space", lazy.layout.next()),
     Key([mod, "shift"], "h", lazy.layout.shuffle_left()),
