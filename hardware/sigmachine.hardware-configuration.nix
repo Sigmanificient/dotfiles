ded59dfb4d8ee3e99842813d363a7efe2d21d103
Sigmanificient <edhyjox@gmail.com>
2025-09-29 05:46:38 +0200

    Format hardware configurations

--- hardware/sigmachine.hardware-configuration.nix
+++ hardware/sigmachine.hardware-configuration.nix
@@ -5,7 +5,8 @@
 
 {
   imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
+    [
+      (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" ];
@@ -14,19 +15,20 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/6ad62d4c-97f4-40da-b0b3-2030dd8f74c0";
+    {
+      device = "/dev/disk/by-uuid/6ad62d4c-97f4-40da-b0b3-2030dd8f74c0";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/DAAD-3276";
+    {
+      device = "/dev/disk/by-uuid/DAAD-3276";
       fsType = "vfat";
       options = [ "fmask=0022" "dmask=0022" ];
     };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/c797aedc-1bdb-4f86-bc35-ad5a9688e057"; }
-    ];
+    [{ device = "/dev/disk/by-uuid/c797aedc-1bdb-4f86-bc35-ad5a9688e057"; }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
--- hardware/toaster.hardware-configuration.nix
+++ hardware/toaster.hardware-configuration.nix
@@ -5,7 +5,8 @@
 
 {
   imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
+    [
+      (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" "sr_mod" "rtsx_usb_sdmmc" ];
@@ -14,12 +15,14 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/7a86598c-ab93-436c-b9de-45170ed248c7";
+    {
+      device = "/dev/disk/by-uuid/7a86598c-ab93-436c-b9de-45170ed248c7";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/D129-5111";
+    {
+      device = "/dev/disk/by-uuid/D129-5111";
       fsType = "vfat";
       options = [ "fmask=0022" "dmask=0022" ];
     };
