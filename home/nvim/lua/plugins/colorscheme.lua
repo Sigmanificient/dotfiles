8a76a7bf6b169a785ff3b1b622180d537340f982
Sigmanificient <edhyjox@gmail.com>
2026-05-29 14:21:14 +0200

    ~/nvim: fix catppuccin project

--- home/nvim/lua/plugins/colorscheme.lua
+++ home/nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,13 @@
 local is_vt_tty = vim.fn.expand("$TERM") == "linux"
 
 if not is_vt_tty then
-  vim.cmd('colorscheme catppuccin-macchiato')
+  require("catppuccin").setup({
+    integrations = {
+      blink_indent = false,
+    }
+  })
+
+  vim.cmd.colorscheme("catppuccin")
 end
 
 vim.api.nvim_set_hl(0, "Normal", { ctermbg = nil })
